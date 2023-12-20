e1324ce9720cf22e5da92ea846f104b2ebb451f1
Pedro Lugon <pedromotta20@gmail.com>
2023-12-20 13:03:20 -0300

    feat(404): custom not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div className="defaultContainer flex flex-col items-center justify-center">
+      <h1 className="">404</h1>
+      <h2 className="">Página não encontrada</h2>
+    </div>
+  );
+}
