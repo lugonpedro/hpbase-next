4124c87f6d359335da48bc47acc023ccd5070172
Pedro Lugon <pedromotta20@gmail.com>
2023-12-22 14:13:36 -0300

    feat(card): all cards working

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,15 +4,85 @@ import Link from "next/link";
 
 export function BookCard(book: BookProps) {
   return (
-    <Link href={`/book/${book.attributes.slug}`}>
+    <Link href={`/books/${book.attributes.slug}`}>
       <Image
         src={book.attributes.cover}
         alt={book.attributes.slug}
         width={300}
         height={300}
-        className="w-full h-auto rounded-xl"
+        className="w-full h-full rounded-xl object-cover object-top"
       />
-      <p></p>
+    </Link>
+  );
+}
+
+export function CharacterCard(character: CharacterProps) {
+  return (
+    <Link
+      href={`/characters/${character.attributes.slug}`}
+      className="flex flex-col gap-4"
+    >
+      <Image
+        src={character.attributes.image ?? "/dementor.png"}
+        alt={character.attributes.slug}
+        width={300}
+        height={300}
+        className="w-full h-[300px] rounded-xl object-cover object-top"
+      />
+      <p className="text-center">{character.attributes.name}</p>
+    </Link>
+  );
+}
+
+export function MovieCard(movie: MovieProps) {
+  return (
+    <Link
+      href={`/movies/${movie.attributes.slug}`}
+      className="flex flex-col gap-4"
+    >
+      <Image
+        src={movie.attributes.poster}
+        alt={movie.attributes.slug}
+        width={300}
+        height={300}
+        className="w-full h-full rounded-xl object-cover object-top"
+      />
+    </Link>
+  );
+}
+
+export function PotionCard(potion: PotionProps) {
+  return (
+    <Link
+      href={`/potions/${potion.attributes.slug}`}
+      className="flex flex-col gap-4"
+    >
+      <Image
+        src={potion.attributes.image ?? "/dementor.png"}
+        alt={potion.attributes.slug}
+        width={300}
+        height={300}
+        className="w-full h-[300px] rounded-xl object-cover object-top"
+      />
+      <p className="text-center">{potion.attributes.name}</p>
+    </Link>
+  );
+}
+
+export function SpellCard(spell: SpellProps) {
+  return (
+    <Link
+      href={`/spells/${spell.attributes.slug}`}
+      className="flex flex-col gap-4"
+    >
+      <Image
+        src={spell.attributes.image ?? "/dementor.png"}
+        alt={spell.attributes.slug}
+        width={300}
+        height={300}
+        className="w-full h-[300px] rounded-xl object-cover object-top"
+      />
+      <p className="text-center">{spell.attributes.name}</p>
     </Link>
   );
 }
