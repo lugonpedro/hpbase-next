108c7df72bd6ec414133a0aa2e2f0c48ab3900b0
Pedro Lugon <pedromotta20@gmail.com>
2023-12-22 17:36:23 -0300

    feat(card tests): all cards tests

--- components/__tests__/Card.spec.tsx
+++ components/__tests__/Card.spec.tsx
@@ -1,23 +1,29 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { BookCard } from "../Card";
+import {
+  BookCard,
+  CharacterCard,
+  MovieCard,
+  PotionCard,
+  SpellCard,
+} from "../Card";
 
 describe("BookCard test", () => {
-  it("should make a book card", () => {
+  it("should make a card", () => {
     const book: BookProps = {
       id: "123",
       type: "book",
       attributes: {
         slug: "book",
-        author: "Author",
+        author: "",
         cover: "/dementor.png",
-        dedication: "Dedication",
+        dedication: "",
         pages: 0,
-        release_date: "2000-01-01",
-        summary: "Summary",
-        title: "Title",
-        wiki: "Wiki",
+        release_date: "",
+        summary: "",
+        title: "",
+        wiki: "",
       },
       relationships: {
         chapters: {
@@ -32,6 +38,148 @@ describe("BookCard test", () => {
     render(<BookCard {...book} />);
 
     const myElem = screen.getByAltText("book");
+    expect(myElem).toBeInTheDocument();
+  });
+});
+
+describe("CharacterCard test", () => {
+  it("should make a card", () => {
+    const character: CharacterProps = {
+      id: "123",
+      type: "character",
+      attributes: {
+        slug: "character",
+        alias_names: [],
+        animagus: "",
+        blood_status: "",
+        boggart: "",
+        born: "",
+        died: "",
+        eye_color: "",
+        family_members: [],
+        gender: "",
+        hair_color: "",
+        height: "",
+        house: "",
+        image: "/dementor.png",
+        jobs: [],
+        marital_status: "",
+        name: "Personagem 1",
+        nationality: "",
+        patronus: "",
+        romances: [],
+        skin_color: "",
+        species: "",
+        titles: [],
+        wands: [],
+        weight: "",
+        wiki: "",
+      },
+      links: {
+        self: "",
+      },
+    };
+
+    render(<CharacterCard {...character} />);
+
+    const myElem = screen.getByText("Personagem 1");
+    expect(myElem).toBeInTheDocument();
+  });
+});
+
+describe("MovieCard test", () => {
+  it("should make a card", () => {
+    const movie: MovieProps = {
+      id: "123",
+      type: "movie",
+      attributes: {
+        slug: "movie",
+        box_office: "",
+        budget: "",
+        cinematographers: [],
+        directors: [],
+        distributors: [],
+        editors: [],
+        music_composers: [],
+        poster: "/dementor.png",
+        producers: [],
+        rating: "",
+        release_date: "",
+        running_time: "",
+        screenwriters: [],
+        summary: "",
+        title: "",
+        trailer: "",
+        wiki: "",
+      },
+      links: {
+        self: "",
+      },
+    };
+
+    render(<MovieCard {...movie} />);
+
+    const myElem = screen.getByAltText("movie");
+    expect(myElem).toBeInTheDocument();
+  });
+});
+
+describe("PotionCard test", () => {
+  it("should make a card", () => {
+    const potion: PotionProps = {
+      id: "123",
+      type: "potion",
+      attributes: {
+        slug: "potion",
+        characteristics: "",
+        difficulty: "",
+        effect: "",
+        image: "/dementor.png",
+        inventors: "",
+        ingredients: "",
+        manufacturers: "",
+        name: "Poção 1",
+        side_effects: "",
+        time: "",
+        wiki: "",
+      },
+      links: {
+        self: "",
+      },
+    };
+
+    render(<PotionCard {...potion} />);
+
+    const myElem = screen.getByText("Poção 1");
+    expect(myElem).toBeInTheDocument();
+  });
+});
+
+describe("SpellCard test", () => {
+  it("should make a card", () => {
+    const spell: SpellProps = {
+      id: "123",
+      type: "spell",
+      attributes: {
+        slug: "spell",
+        category: "",
+        creator: "",
+        effect: "",
+        hand: "",
+        image: "/dementor.png",
+        incantation: "",
+        light: "",
+        name: "Magia 1",
+        wiki: "",
+      },
+      links: {
+        self: "",
+      },
+    };
+
+    render(<SpellCard {...spell} />);
+
+    const myElem = screen.getByText("Magia 1");
     expect(myElem).toBeInTheDocument();
   });
 });
