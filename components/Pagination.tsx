10da9de8a65b756fab5319913fcabe3d82802a36
Pedro Lugon <pedromotta20@gmail.com>
2023-12-27 11:40:03 -0300

    fix(pagination): dont show pagination when searching

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -10,21 +10,23 @@ interface PaginationProps {
 export function Pagination(props: PaginationProps) {
   return (
     <nav>
-      <ul className="flex flex-row items-center justify-center gap-4 mt-4">
-        <Button
-          label="Anterior"
-          onClick={props.onClickPreviousButton}
-          disabled={props.actualPage === 1}
-        />
-        <p>
-          {props.actualPage}/{props.totalPages}
-        </p>
-        <Button
-          label="Próxima"
-          onClick={props.onClickNextButton}
-          disabled={props.actualPage === props.totalPages}
-        />
-      </ul>
+      {props.totalPages && (
+        <ul className="flex flex-row items-center justify-center gap-4 mt-4">
+          <Button
+            label="Anterior"
+            onClick={props.onClickPreviousButton}
+            disabled={props.actualPage === 1}
+          />
+          <p>
+            {props.actualPage}/{props.totalPages}
+          </p>
+          <Button
+            label="Próxima"
+            onClick={props.onClickNextButton}
+            disabled={props.actualPage === props.totalPages}
+          />
+        </ul>
+      )}
     </nav>
   );
 }
